8fde06ffd193f15dd543ad0605177299b81f1a57
Gabriela2009-gif <gabriela.peske@escola.pr.gov.br>
2026-09-24 10:06:43 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -48,44 +48,4 @@ export const perguntas = [
                
             ]
         },
-]
-let atual = 0;
-let perguntaAtual;
-let historiaFinal = "";
-
-function mostraPergunta(){
-    if(atual >= perguntas.length){
-        mostraResultado();
-        return;
-    }
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "";
-    mostraAlternativas();
-}
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas);
-    }
-function aleatorio(lista) {
-const posicao = Math.floor(Math.random()* lista.length);
-}
-}
-function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
-    historiaFinal += afirmacoes + " ";
-    atual++
-    mostraPergunta();
-}
-function mostraResultado(){
-    caixaPerguntas.textContent = "Olha só o que podemos afirmar sobre você...";
-    textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = "";
-}
-
-
-
-mostraPergunta();
+];
